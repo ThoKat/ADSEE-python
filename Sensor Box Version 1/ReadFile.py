e33a25e8d080f9d30f07c6c17bdcaff5ec413a1a
ThoKat <kattenliefhebber1@gmail.com>
2022-04-26 10:57:43 +0200

    Files Reading

---
+++ Sensor Box Version 1/ReadFile.py
@@ -0,0 +1,15 @@
+# Read our file into a list of lines
+with open('test.dat', 'r') as test:
+    # Get a single-line header from our file
+    header = test.readline()
+    for line in test:
+        # print(line)
+        
+        # Split into the part before and after the equals sign
+        values = line.split(',')
+        # The first word is the name; remove potential whitespace
+        load_cell = values[0].strip()
+        # The second word is the value, which needs more processing
+        time_of_flight = values[1].strip()
+
+        print(f'load cell: {load_cell} and time of flight: {time_of_flight}')
